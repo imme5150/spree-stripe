dc8950d6da8b540d36d420b8748d97afc7ffa10f
liu fengyun <liufengyunchina@gmail.com>
2013-03-31 19:07:44 +0800

    fix exception in rails 3.1+ and spree 0.7x
    In spree 0.7x, there's following excpetion:
    
        app/models/spree/gateway/stripe.rb:1:in `<top (required)>': wrong argument type Module (expected Class)
    
    In rails 3.1+, there's issue with mass assignment.

--- app/models/spree/gateway/stripe.rb
+++ app/models/spree/gateway/stripe.rb
@@ -1,16 +1,14 @@
-module Spree
-  class Gateway::Stripe < Gateway
-    preference :login, :string
-    attr_accessible :preferred_login, :preferred_password
+class Spree::Gateway::Stripe < Gateway
+  preference :login, :string
+  attr_accessible :preferred_login, :preferred_password, :name, :description, :environment, :display_on, :active
 
-    # Make sure to have Spree::Config[:auto_capture] set to true.
+  # Make sure to have Spree::Config[:auto_capture] set to true.
 
-    def provider_class
-      ActiveMerchant::Billing::StripeGateway
-    end
+  def provider_class
+    ActiveMerchant::Billing::StripeGateway
+  end
 
-    def payment_profiles_supported?
-      false
-    end
+  def payment_profiles_supported?
+    false
   end
 end
