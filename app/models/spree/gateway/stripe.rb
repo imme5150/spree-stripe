e78179093169797dafccf22037ba35ab4694f261
Josh Shupack <ynaught@gmail.com>
2017-02-01 11:51:41 -0800

    attempt to fix namespace

--- app/models/spree/gateway/stripe.rb
+++ app/models/spree/gateway/stripe.rb
@@ -1,52 +1,50 @@
-module Spree
-  class Gateway::Stripe < Gateway
-    preference :login, :string
+class Spree::Gateway::Stripe < Gateway
+  preference :login, :string
 
-    def provider_class
-      ActiveMerchant::Billing::StripeGateway
+  def provider_class
+    ActiveMerchant::Billing::StripeGateway
+  end
+
+  def payment_profiles_supported?
+    true
+  end
+
+  def purchase(money, creditcard, gateway_options)
+    options = {}
+    options[:description] ||= "Spree Order ID: #{gateway_options[:order_id]}"
+    if customer = creditcard.gateway_customer_profile_id
+      options[:customer] = customer
+      creditcard = nil
     end
+    provider.purchase(money, creditcard, options)
+  end
 
-    def payment_profiles_supported?
-      true
-    end
+  def authorize(money, creditcard, gateway_options)
+    raise "The spree-stripe gem does not currently support separate auth and capture. Either update the gem if you need this functionality or set Spree::Config[:auto_capture] to true"
+  end
 
-    def purchase(money, creditcard, gateway_options)
-      options = {}
-      options[:description] ||= "Spree Order ID: #{gateway_options[:order_id]}"
-      if customer = creditcard.gateway_customer_profile_id
-        options[:customer] = customer
-        creditcard = nil
-      end
-      provider.purchase(money, creditcard, options)
-    end
+  def capture(authorization, creditcard, gateway_options)
+    raise "The spree-stripe gem does not currently support separate auth and capture. Either update the gem if you need this functionality or set Spree::Config[:auto_capture] to true"
+  end
 
-    def authorize(money, creditcard, gateway_options)
-      raise "The spree-stripe gem does not currently support separate auth and capture. Either update the gem if you need this functionality or set Spree::Config[:auto_capture] to true"
-    end
+  def credit(money, creditcard, response_code, gateway_options)
+    provider.credit(money, response_code, {})
+  end
 
-    def capture(authorization, creditcard, gateway_options)
-      raise "The spree-stripe gem does not currently support separate auth and capture. Either update the gem if you need this functionality or set Spree::Config[:auto_capture] to true"
-    end
+  def void(response_code, gateway_options)
+    provider.void(response_code, {})
+  end
 
-    def credit(money, creditcard, response_code, gateway_options)
-      provider.credit(money, response_code, {})
-    end
+  def create_profile(payment)
+    return unless payment.source.gateway_customer_profile_id.nil?
 
-    def void(response_code, gateway_options)
-      provider.void(response_code, {})
-    end
-
-    def create_profile(payment)
-      return unless payment.source.gateway_customer_profile_id.nil?
-
-      options = {}
-      options[:email] = payment.order.email
-      response = provider.store(payment.source, options)
-      if response.success?
-        payment.source.update_attributes!(:gateway_customer_profile_id => response.params['id'])
-      else
-        payment.source.gateway_error(response.message)
-      end
+    options = {}
+    options[:email] = payment.order.email
+    response = provider.store(payment.source, options)
+    if response.success?
+      payment.source.update_attributes!(:gateway_customer_profile_id => response.params['id'])
+    else
+      payment.source.gateway_error(response.message)
     end
   end
 end
